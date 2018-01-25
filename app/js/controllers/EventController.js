e7039c0e4bd93025d1bae4bb7952f44df7c47fe2
Shaikh, Noor Mohammed <ShaikhN@autonation.com>
2018-01-25 00:23:45 -0500

    ng-class

--- app/js/controllers/EventController.js
+++ app/js/controllers/EventController.js
@@ -4,7 +4,8 @@ eventsApp.controller('EventController',
         function EventController($scope){
             $scope.snippet = '<span style="color-red">hi there</span>';
             $scope.boolValue = true;
-            $scope.mystyle = {color:'red'};
+            $scope.myStyle = {color:'red'};
+            $scope.myClass = 'blue';
             $scope.event = {
                 name: "Angular boot camp",
                 date: '1/1/2017',
