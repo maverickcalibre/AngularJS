611a7e44284daace97960f99577bbcec6f6a3b54
Shaikh, Noor Mohammed <ShaikhN@autonation.com>
2018-01-28 23:36:53 -0500

    ng-pattern validation

--- app/js/controllers/EditEventController.js
+++ app/js/controllers/EditEventController.js
@@ -2,8 +2,11 @@
 
 eventsApp.controller('EditEventController',
     function EditEventController($scope){
-        $scope.saveEvent=function(event){
-            alert(event.name);
+        $scope.saveEvent=function(event, newEventForm){
+            console.log(newEventForm);
+            if(newEventForm.$valid){
+                alert(event.name);
+            }
         }
 
         $scope.cancelEdit=function(){
